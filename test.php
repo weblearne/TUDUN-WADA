e521178345fd3a29e2a03151a593fc465fadeba1
Basiru Lawan <79064498+weblearne@users.noreply.github.com>
2025-06-04 22:44:25 +0100

    Add files via upload

---
+++ test.php
@@ -0,0 +1,12 @@
+<?php 
+
+ if ($_SERVER['REQUEST_METHOD']== POST) {
+
+ 	$firstname = htmlspecialchars($_POST['firstname']);
+ 	$firstname = htmlspecialchars($_POST['firstname']);
+ 	$firstname = htmlspecialchars($_POST['firstname']);
+ }
+
+
+
+ ?>
